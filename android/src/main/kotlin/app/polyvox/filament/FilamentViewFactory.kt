6bc305cda2f7757526a72bcf3995f35b4b4f97f9
Nick Fisher <nick.fisher@avinium.com>
2022-04-05 13:31:38 +1000

    ViewFactory fix for newer versions of Flutter

--- android/src/main/kotlin/app/polyvox/filament/FilamentViewFactory.kt
+++ android/src/main/kotlin/app/polyvox/filament/FilamentViewFactory.kt
@@ -13,8 +13,8 @@ class FilamentViewFactory(
     private val activity: Activity,
     private val binaryMessenger: BinaryMessenger 
 ) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+    override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
         val creationParams = args as Map<String?, Any?>?
-        return FilamentView(viewId, context, activity, binaryMessenger, creationParams)
+        return FilamentView(viewId, context!!, activity, binaryMessenger, creationParams)
     }
 }
