e39d75824e880303f631721364fc71fa61d43055
Nick Fisher <nick.fisher@avinium.com>
2023-10-24 12:27:48 +1100

    update Windows example project

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <polyvox_filament/polyvox_filament_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   PolyvoxFilamentPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PolyvoxFilamentPluginCApi"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  permission_handler_windows
   polyvox_filament
 )
 
