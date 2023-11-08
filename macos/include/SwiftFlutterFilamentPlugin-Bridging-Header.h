a0671a9b6f084ee02f1f5b7000e34f884fd27241
Daverin <mail@daverin.net>
2023-11-08 16:51:36 +0200

    fix: remove macOS/include and macOS/src from git feat: makefile shortcuts for syncing macOS libs and generating background material fix: update gitattributes to be more managable docs: update README with new makefile shortcuts

--- macos/include/SwiftFlutterFilamentPlugin-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#ifndef SwiftFlutterFilamentPlugin_Bridging_Header_h
-#define SwiftFlutterFilamentPlugin_Bridging_Header_h
-
-#import "FlutterFilamentApi.h"
-#import "FlutterFilamentFFIApi.h"
-
-#endif
-
