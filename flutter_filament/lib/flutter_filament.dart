a7fb516ff3aeda0ea839f2b23a2a5d21145caafc
Nick Fisher <nick.fisher@avinium.com>
2024-05-10 23:04:22 +0800

    remove outdated export

--- flutter_filament/lib/flutter_filament.dart
+++ flutter_filament/lib/flutter_filament.dart
@@ -6,7 +6,6 @@ export 'filament/widgets/camera/camera_options_widget.dart';
 export 'filament/widgets/camera/gestures/filament_gesture_detector.dart';
 export 'filament/widgets/filament_widget.dart';
 export 'filament/widgets/debug/entity_list_widget.dart';
-export 'filament/entities/entity_transform_controller.dart';
 export 'filament/widgets/camera/entity_controller_mouse_widget.dart';
 export 'package:dart_filament/dart_filament.dart';
 
