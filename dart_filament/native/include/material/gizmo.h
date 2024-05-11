f7c3d20aa96f586dc44bb3e9b19d227eb0eaef4b
Nick Fisher <nick.fisher@avinium.com>
2024-05-11 22:38:52 +0800

    add guards for material headers

--- dart_filament/native/include/material/gizmo.h
+++ dart_filament/native/include/material/gizmo.h
@@ -3,12 +3,16 @@
 
 #include <stdint.h>
 
+#if defined(__cplusplus)
 extern "C"
 {
+#endif
     extern const uint8_t GIZMO_PACKAGE[];
     extern int GIZMO_GIZMO_OFFSET;
     extern int GIZMO_GIZMO_SIZE;
+#if defined(__cplusplus)
 }
+#endif
 #define GIZMO_GIZMO_DATA (GIZMO_PACKAGE + GIZMO_GIZMO_OFFSET)
 
 #endif
--- dart_filament/native/include/material/image.h
+++ dart_filament/native/include/material/image.h
@@ -3,12 +3,16 @@
 
 #include <stdint.h>
 
+#if defined(__cplusplus)
 extern "C"
 {
+#endif
     extern const uint8_t IMAGE_PACKAGE[];
     extern int IMAGE_IMAGE_OFFSET;
     extern int IMAGE_IMAGE_SIZE;
+#if defined(__cplusplus)
 }
+#endif
 #define IMAGE_IMAGE_DATA (IMAGE_PACKAGE + IMAGE_IMAGE_OFFSET)
 
 #endif
