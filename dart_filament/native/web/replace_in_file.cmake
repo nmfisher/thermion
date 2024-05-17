b8588e3196d706a196513be54c927c055761ff3e
Nick Fisher <nick.fisher@avinium.com>
2024-05-17 14:33:24 +0800

    dont use lists for CMake replace in file

--- dart_filament/native/web/replace_in_file.cmake
+++ dart_filament/native/web/replace_in_file.cmake
@@ -1,14 +1,3 @@
 file(READ ${INPUTFILE} INPUT_TEXT)
-
-foreach(TO_REPLACE_STRING ${TO_REPLACE})
-
-    #get the index of the current to_replace string
-    list(FIND TO_REPLACE ${TO_REPLACE_STRING} REPLACE_INDEX)
-    
-    #look up the corresponding replacement string
-    list(GET REPLACEMENT ${REPLACE_INDEX} REPLACEMENT_STRING)
-
-    string(REPLACE ${TO_REPLACE_STRING} ${REPLACEMENT_STRING} INPUT_TEXT "${INPUT_TEXT}")
-endforeach()
-
+string(REPLACE ${TO_REPLACE} ${REPLACEMENT} INPUT_TEXT "${INPUT_TEXT}")
 file(WRITE ${OUTPUTFILE} "${INPUT_TEXT}")
