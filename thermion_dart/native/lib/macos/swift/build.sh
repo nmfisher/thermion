0cdc01549a96f0fbf147da8d3e271250fdf2f973
Nick Fisher <nick.fisher@avinium.com>
2024-06-26 15:57:45 +1000

    re-add non-LFS versions

---
+++ thermion_dart/native/lib/macos/swift/build.sh
@@ -0,0 +1,1 @@
+swiftc -c ../../../../../thermion_flutter/thermion_flutter/macos/Classes/ThermionDartTexture.swift -module-name thermion_dart_texture -emit-objc-header-path ./ThermionDartTexture.h -emit-library -o ./libthermion_swift.dylib
