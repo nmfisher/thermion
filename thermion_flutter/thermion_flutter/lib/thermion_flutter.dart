8963b79db0aca07021191ebf8750e5099f4bd348
Nick Fisher <nick.fisher@avinium.com>
2024-09-05 17:43:58 +0800

    chore: (flutter) export platform interface from thermion_flutter package

--- thermion_flutter/thermion_flutter/lib/thermion_flutter.dart
+++ thermion_flutter/thermion_flutter/lib/thermion_flutter.dart
@@ -3,5 +3,5 @@ library thermion_flutter;
 export 'thermion/thermion_flutter_plugin.dart';
 export 'thermion/widgets/thermion_widget.dart';
 export 'thermion/widgets/camera/gestures/thermion_gesture_detector.dart';
-
+export 'package:thermion_flutter_platform_interface/thermion_flutter_platform_interface.dart';
 export 'package:thermion_dart/thermion_dart.dart';
