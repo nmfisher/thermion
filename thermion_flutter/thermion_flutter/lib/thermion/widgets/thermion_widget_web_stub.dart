03ffe8511344e3387859cd9022551a85eeacb44f
Nick Fisher <nick.fisher@avinium.com>
2024-09-05 17:51:36 +0800

    chore: stub ThermionWidget methods

--- thermion_flutter/thermion_flutter/lib/thermion/widgets/thermion_widget_web_stub.dart
+++ thermion_flutter/thermion_flutter/lib/thermion/widgets/thermion_widget_web_stub.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:thermion_flutter_ffi/thermion_flutter_ffi.dart';
+import 'package:thermion_flutter_web/thermion_flutter_web_options.dart';
 
 class ThermionWidgetWeb extends StatefulWidget {
+  final ThermionFlutterWebOptions options;
+
+  const ThermionWidgetWeb({super.key, required this.options});
+  
   @override
   State<StatefulWidget> createState() => throw Exception();
 }
