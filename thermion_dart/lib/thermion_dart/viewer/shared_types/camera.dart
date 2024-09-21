493d547c7bab1b0c57b66a89696d26fdbbf1a824
Nick Fisher <nick.fisher@avinium.com>
2024-09-21 10:18:36 +0800

    add Dart Camera type

---
+++ thermion_dart/lib/thermion_dart/viewer/shared_types/camera.dart
@@ -0,0 +1,6 @@
+// abstract class Camera {
+//   Future setProjectionMatrixWithCulling(Matrix4 projectionMatrix, Matrix4 projectionMatrixForCUlling);
+//   Future setDepthCullingEnabled(bool enabled);
+// }
+
+enum AlphaMode { OPAQUE, MASK, BLEND }
