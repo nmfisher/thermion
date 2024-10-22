a08620d7c99e66377a196880a1d9ae2f79ff2bfa
Nick Fisher <nick.fisher@avinium.com>
2024-10-22 22:23:13 +0800

    rename gestureHandler to inputHandler

--- thermion_dart/lib/src/input/input.dart
+++ thermion_dart/lib/src/input/input.dart
@@ -2,6 +2,6 @@ library;
 
 export 'src/input_handler.dart';
 export 'src/delegates.dart';
-export 'src/delegate_gesture_handler.dart';
+export 'src/delegate_input_handler.dart';
 export 'src/implementations/default_pick_delegate.dart';
 export 'src/implementations/third_person_camera_delegate.dart';
