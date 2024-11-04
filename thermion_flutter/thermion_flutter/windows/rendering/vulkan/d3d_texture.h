b59cadc061d2c9d3a11748b346c3c4045928422a
Nick Fisher <nick.fisher@avinium.com>
2024-11-04 23:50:52 +0800

    add first pass vulkan implementation

---
+++ thermion_flutter/thermion_flutter/windows/rendering/vulkan/d3d_texture.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <functional>
+#include <mutex>
+
+#include <d3d.h>
+#include <d3d11.h>
+#include <dxgi1_2.h>  // Add this line
+#include <d3d11_4.h>
+#include <Windows.h>
+#include <wrl.h>
+
+typedef uint32_t GLuint;
+
+namespace thermion::windows::d3d {
+
+class D3DTexture {
+  public:
+    D3DTexture(
+        uint32_t width,
+        uint32_t height,
+        std::function<void(size_t, size_t)> onResizeRequested
+    );    
+    ~D3DTexture();
+
+    void Flush();
+    HANDLE GetTextureHandle();
+   
+    GLuint glTextureId = 0;
+
+    void FillBlueAndSaveToBMP(const char* filename);
+    bool SaveTextureAsBMP(ID3D11Texture2D* texture, const char* filename);
+    // Device
+    ID3D11Device* _D3D11Device = nullptr;
+      
+  private:
+    bool _error = false;
+    uint32_t _width = 0;
+    uint32_t _height = 0;
+    bool logged = false;
+    std::function<void(size_t, size_t)> _onResizeRequested;
+
+
+    ID3D11DeviceContext* _D3D11DeviceContext = nullptr;
+    // Texture objects/shared handles
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> _d3dTexture2D;
+    HANDLE _d3dTexture2DHandle = nullptr;
+    
+};
+
+}
