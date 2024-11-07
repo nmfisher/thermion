3c9443328cea104598b3658ac0210ad5b6fa25c1
Nick Fisher <nick.fisher@avinium.com>
2024-11-07 16:35:10 +0800

    move Windows engine to Vulkan backend and flush after creating swapchain

--- thermion_dart/native/src/FilamentViewer.cpp
+++ thermion_dart/native/src/FilamentViewer.cpp
@@ -127,8 +127,7 @@ namespace thermion
   FilamentViewer::FilamentViewer(const void *sharedContext, const ResourceLoaderWrapperImpl *const resourceLoader, void *const platform, const char *uberArchivePath)
       : _resourceLoaderWrapper(resourceLoader)
   {
-    Log("Creating engine wiht sharedContext %d", sharedContext);
-    
+   
     ASSERT_POSTCONDITION(_resourceLoaderWrapper != nullptr, "Resource loader must be non-null");
 
 #if TARGET_OS_IPHONE
@@ -140,13 +139,11 @@ namespace thermion
 #elif defined(__EMSCRIPTEN__)
     _engine = Engine::create(Engine::Backend::OPENGL, (backend::Platform *)new filament::backend::PlatformWebGL(), (void *)sharedContext, nullptr);
 #elif defined(_WIN32)
-    Engine::Config config;
-    config.stereoscopicEyeCount = 1;
-    _engine = Engine::create(Engine::Backend::OPENGL, (backend::Platform *)platform, (void *)sharedContext, &config);
+    _engine = Engine::create(Engine::Backend::VULKAN, (backend::Platform *)platform, (void *)sharedContext, nullptr);
 #else
     _engine = Engine::create(Engine::Backend::OPENGL, (backend::Platform *)platform, (void *)sharedContext, nullptr);
-#endif
-    Log("Engine created");
+ #endif
+
     _engine->setAutomaticInstancingEnabled(true);
 
     _renderer = _engine->createRenderer();
@@ -685,6 +682,7 @@ namespace thermion
     SwapChain *swapChain;
     swapChain = _engine->createSwapChain(width, height, filament::backend::SWAP_CHAIN_CONFIG_TRANSPARENT | filament::backend::SWAP_CHAIN_CONFIG_READABLE | filament::SwapChain::CONFIG_HAS_STENCIL_BUFFER);
     _swapChains.push_back(swapChain);
+    _engine->flushAndWait();
     return swapChain;
   }
 
