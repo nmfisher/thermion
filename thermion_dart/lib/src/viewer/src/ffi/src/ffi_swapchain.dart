09b6aca08f44b056459808ba16ecf7ea2a9ffe81
Nick Fisher <nick.fisher@avinium.com>
2025-03-17 16:39:28 +0800

    move FFISwapChain to own file

---
+++ thermion_dart/lib/src/viewer/src/ffi/src/ffi_swapchain.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+import 'package:thermion_dart/src/viewer/src/ffi/src/callbacks.dart';
+import 'package:thermion_dart/thermion_dart.dart';
+
+class FFISwapChain extends SwapChain {
+  final Pointer<TSwapChain> swapChain;
+  final Pointer<TViewer> viewer;
+
+  FFISwapChain(this.swapChain, this.viewer);
+}
