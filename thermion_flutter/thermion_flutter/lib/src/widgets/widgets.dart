4bdbb170a7b1619a09586d31865322c614ad8711
Nick Fisher <nick.fisher@avinium.com>
2025-03-28 11:48:39 +0800

    export ViewerWidget

--- thermion_flutter/thermion_flutter/lib/src/widgets/widgets.dart
+++ thermion_flutter/thermion_flutter/lib/src/widgets/widgets.dart
@@ -1,6 +1,7 @@
 library;
 
 export 'src/thermion_widget.dart';
+export 'src/viewer_widget.dart';
 export 'src/thermion_listener_widget.dart';
 export 'src/camera/camera_selector_widget.dart';
 export 'src/camera/camera_orientation_widget.dart';
