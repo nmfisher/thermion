8a44cbdebc3a027699046d2e721e5dd1d3436dad
Nick Fisher <nick.fisher@avinium.com>
2025-04-28 16:33:10 +0800

    update js_wasm main.js for node

--- examples/dart/js_wasm/bin/main.js
+++ examples/dart/js_wasm/bin/main.js
@@ -3,14 +3,19 @@ import { readFile } from 'fs/promises';
 import { compile } from './example.mjs';
 import thermion_dart from './thermion_dart.js';
 
-globalThis['thermion_dart'] = await thermion_dart(); 
-
-const wasmBytes = await readFile('example.wasm');
 
 async function runDartWasm() {
+    globalThis['thermion_dart'] = await thermion_dart(); 
+
+    const wasmBytes = await readFile('example.wasm');
     const compiledApp = await compile(wasmBytes);
     const instantiatedApp = await compiledApp.instantiate({});
-    instantiatedApp.invokeMain();
+    try {
+        instantiatedApp.invokeMain();
+    } catch(err) {
+        console.error("Failed");
+        console.error(err);
+    }
 }
 
 runDartWasm().catch(console.error);
