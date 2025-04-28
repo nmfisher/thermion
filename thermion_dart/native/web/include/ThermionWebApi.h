62d8ab62a3034dcf21da868697959899bf74f37c
Nick Fisher <nick.fisher@avinium.com>
2025-04-28 16:28:14 +0800

    redefine EMSCRIPTEN_WEBGL_CONTEXT_HANDLE for jsgen

--- thermion_dart/native/web/include/ThermionWebApi.h
+++ thermion_dart/native/web/include/ThermionWebApi.h
@@ -7,6 +7,13 @@
 #include <emscripten/emscripten.h>
 #include <emscripten/html5_webgl.h>
 
+// when re-generating JS interop bindings, 
+// we may not know exactly where the emscripten headers
+// are, so we define this manually just to ensure codegen works.
+#ifndef EMSCRIPTEN_WEBGL_CONTEXT_HANDLE
+typedef uintptr_t EMSCRIPTEN_WEBGL_CONTEXT_HANDLE;
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
