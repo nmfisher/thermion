9240a042cd65f92cd715624a8d737c80dbf507b1
Nick Fisher <nick.fisher@avinium.com>
2025-05-01 17:21:34 +0800

    cleanup web api header

--- thermion_dart/native/web/include/ThermionWebApi.h
+++ thermion_dart/native/web/include/ThermionWebApi.h
@@ -1,38 +1,14 @@
 #pragma once
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-
-// obviously __EMSCRIPTEN__ should always be true when 
-// compiling this 
-// just to satisfy code-gen
-#ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
 #include <emscripten/html5_webgl.h>
-#endif 
-
-// when re-generating JS interop bindings, 
-// we may not know exactly where the emscripten headers
-// are, so we define this manually just to ensure codegen works.
-#ifndef EMSCRIPTEN_WEBGL_CONTEXT_HANDLE
-typedef uintptr_t EMSCRIPTEN_WEBGL_CONTEXT_HANDLE;
-#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-EMSCRIPTEN_WEBGL_CONTEXT_HANDLE Thermion_createGLContext(
-    // bool alpha,
-    // bool depth,
-    // bool stencil,
-    // bool antiAlias,
-    // bool explicitSwapControl,
-    // bool preserveDrawingBuffer,
-    // int proxyMode,
-    // bool renderViaOffscreenBackBuffer
-);
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE Thermion_createGLContext();
+
 
 #ifdef __cplusplus
 }
