f806827c0d1624a73e0768fc1821dd8d31819a62
Nick Fisher <nick.fisher@avinium.com>
2025-05-06 15:19:01 +0800

    add defaultResourceLoader for JS platforms (http)

--- thermion_dart/lib/src/filament/src/implementation/resource_loader_js.dart
+++ thermion_dart/lib/src/filament/src/implementation/resource_loader_js.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
 import 'package:thermion_dart/thermion_dart.dart';
+import 'package:http/http.dart' as http;
 
-Future<Uint8List> defaultResourceLoader(String path) {
+Future<Uint8List> defaultResourceLoader(String path) async {
     if(path.startsWith("file://")) {
       throw Exception("Unsupported URI : $path");
     }
-    return File(path).readAsBytes();
+
+    final response = await http.get(Uri.parse(path));
+    return response.bodyBytes;
 }
