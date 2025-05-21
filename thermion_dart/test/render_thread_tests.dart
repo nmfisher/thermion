100845219803da89bb6cb7bd8b25cd8d7346e518
Nick Fisher <nick.fisher@avinium.com>
2025-05-21 13:16:20 +0800

    (test) formatting

--- thermion_dart/test/render_thread_tests.dart
+++ thermion_dart/test/render_thread_tests.dart
@@ -10,26 +10,23 @@ void main() async {
 
   await testHelper.setup();
 
-  group("render thread/capture", () {
-    test("request frame on render thread", () async {
-      await testHelper.withViewer((viewer) async {
-        await viewer.render();
+  test("request frame on render thread", () async {
+    await testHelper.withViewer((viewer) async {
+      await viewer.render();
 
-        await Future.delayed(Duration(milliseconds: 1));
+      await Future.delayed(Duration(milliseconds: 1));
 
-        // var data = viewer.metalTexture.getTextureBytes()!;
-        // var pixels = data.bytes.cast<Uint8>().asTypedList(data.length);
+      // var data = viewer.metalTexture.getTextureBytes()!;
+      // var pixels = data.bytes.cast<Uint8>().asTypedList(data.length);
 
-        // savePixelBufferToBmp(
-        //     pixels, 500, 500, "${testHelper.testDir}/request_frame.bmp");
+      // savePixelBufferToBmp(
+      //     pixels, 500, 500, "${testHelper.testDir}/request_frame.bmp");
 
-        await viewer.setRendering(true);
+      await viewer.setRendering(true);
 
-        await Future.delayed(Duration(seconds: 1));
-        await FilamentApp.instance!.requestFrame();
-        await testHelper.capture(viewer.view, "render_thread_2");
-
-      });
+      await Future.delayed(Duration(seconds: 1));
+      await FilamentApp.instance!.requestFrame();
+      await testHelper.capture(viewer.view, "render_thread_2");
     });
   });
 }
