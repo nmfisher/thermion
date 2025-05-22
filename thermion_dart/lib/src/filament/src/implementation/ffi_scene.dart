5733bc125b6e6e1dbf448822f065b56d1bfbbcdf
Nick Fisher <nick.fisher@avinium.com>
2025-05-22 15:07:43 +0800

    add addEntity method to Scene

--- thermion_dart/lib/src/filament/src/implementation/ffi_scene.dart
+++ thermion_dart/lib/src/filament/src/implementation/ffi_scene.dart
@@ -1,6 +1,6 @@
 import 'package:thermion_dart/src/filament/src/implementation/ffi_asset.dart';
 import 'package:thermion_dart/src/filament/src/interface/scene.dart';
-import 'package:thermion_dart/src/bindings/bindings.dart';
+import 'package:thermion_dart/thermion_dart.dart';
 
 class FFIScene extends Scene {
   final Pointer<TScene> scene;
@@ -10,6 +10,11 @@ class FFIScene extends Scene {
   @override
   Future add(covariant FFIAsset asset) async {
     SceneAsset_addToScene(asset.asset, scene);
+  }
+
+  @override
+  Future addEntity(ThermionEntity entity) async {
+    Scene_addEntity(scene, entity);
   }
 
   @override
--- thermion_dart/lib/src/filament/src/interface/scene.dart
+++ thermion_dart/lib/src/filament/src/interface/scene.dart
@@ -2,5 +2,6 @@ import 'package:thermion_dart/thermion_dart.dart';
 
 abstract class Scene {
   Future add(covariant ThermionAsset asset);
+  Future addEntity(ThermionEntity entity);
   Future remove(covariant ThermionAsset asset);
 }
