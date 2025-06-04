fc4686b20f839180d7b825a465c865876578b988
Nick Fisher <nick.fisher@avinium.com>
2025-06-04 17:44:24 +0800

    add documentation for Scene

--- thermion_dart/lib/src/filament/src/interface/scene.dart
+++ thermion_dart/lib/src/filament/src/interface/scene.dart
@@ -1,7 +1,18 @@
 import 'package:thermion_dart/thermion_dart.dart';
 
 abstract class Scene {
+  
+  /// Adds all renderable entities in [asset] to this scene.
+  ///
+  ///
   Future add(covariant ThermionAsset asset);
+  
+  /// Adds [entity] to this scene (which is assumed to have a Renderable 
+  /// component).
+  ///
   Future addEntity(ThermionEntity entity);
+
+  /// Removes all renderable entities in [asset] from this scene.
+  ///
   Future remove(covariant ThermionAsset asset);
 }
