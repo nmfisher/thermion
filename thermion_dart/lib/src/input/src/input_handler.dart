b6d869275acc578656d3dd0ae76859a8fa147a4c
Nick Fisher <nick.fisher@avinium.com>
2025-06-20 18:33:35 +0800

    InputHandler shouldn't return Future add mousePanSensitivity

--- thermion_dart/lib/src/input/src/input_handler.dart
+++ thermion_dart/lib/src/input/src/input_handler.dart
@@ -5,10 +5,11 @@ import 'input_types.dart';
 /// An interface for handling user device input events.
 ///
 abstract class InputHandler {
+  
   ///
   ///
   ///
-  Future? handle(InputEvent event);
+  void handle(InputEvent event);
 
   ///
   ///
@@ -20,6 +21,7 @@ class InputSensitivityOptions {
   final double touchSensitivity;
   final double touchScaleSensitivity;
   final double mouseSensitivity;
+  final double mousePanSensitivity;
   final double keySensitivity;
   final double scrollWheelSensitivity;
 
@@ -27,6 +29,7 @@ class InputSensitivityOptions {
       {this.touchSensitivity = 0.001,
       this.touchScaleSensitivity = 2.0,
       this.mouseSensitivity = 0.001,
+      this.mousePanSensitivity = 0.01,
       this.scrollWheelSensitivity = 0.01,
       this.keySensitivity = 0.1});
 }
