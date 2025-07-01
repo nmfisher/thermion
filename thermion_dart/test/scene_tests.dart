c0ed7cbc9047b08fa15d5361352bb0e16d901267
Nick Fisher <nick.fisher@avinium.com>
2025-07-01 13:10:02 +0800

    (test) remove stencil highlight from scene_test.dart

--- thermion_dart/test/scene_tests.dart
+++ thermion_dart/test/scene_tests.dart
@@ -6,26 +6,5 @@ void main() async {
   final testHelper = TestHelper("scene");
   await testHelper.setup();
 
-  test('show stencil highlight', () async {
-    await testHelper.withViewer((viewer) async {
-      await viewer.view.setStencilBufferEnabled(true);
-      final cube = await viewer.createGeometry(
-          GeometryHelper.cube(
-            normals: true,
-            uvs: true,
-          ),
-          keepData: true,
-          materialInstances: []);
-
-      await viewer.addToScene(cube);
-
-      final scene = await viewer.view.getScene();
-      await scene.setStencilHighlight(cube);
-
-      await testHelper.capture(viewer.view, "stencil_highlight");
-      await scene.removeStencilHighlight(cube);
-
-      await testHelper.capture(viewer.view, "stencil_highlight_removed");
-    }, createStencilBuffer: true);
-  });
+ 
 }
