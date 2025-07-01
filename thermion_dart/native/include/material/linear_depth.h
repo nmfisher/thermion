4b9870244a529bf086280331d38c55e294425b4f
Nick Fisher <nick.fisher@avinium.com>
2025-07-01 13:16:07 +0800

    fix material headers for web

--- thermion_dart/native/include/material/linear_depth.h
+++ thermion_dart/native/include/material/linear_depth.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     extern const uint8_t LINEAR_DEPTH_PACKAGE[];
     extern int LINEAR_DEPTH_LINEAR_DEPTH_OFFSET;
     extern int LINEAR_DEPTH_LINEAR_DEPTH_SIZE;
+#ifdef __cplusplus
 }
+#endif
 #define LINEAR_DEPTH_LINEAR_DEPTH_DATA (LINEAR_DEPTH_PACKAGE + LINEAR_DEPTH_LINEAR_DEPTH_OFFSET)
 
 #endif
