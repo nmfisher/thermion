b7e79b1ac52f10c0a9511062198b21df7147bcff
Nick Fisher <nick.fisher@avinium.com>
2025-07-01 11:07:01 +0800

    move outline material to embedded binary

--- materials/build.sh
+++ materials/build.sh
@@ -1,4 +1,4 @@
-for material in image unlit_fixed_size grid linear_depth; do \
+for material in image unlit_fixed_size grid linear_depth outline; do \
     echo $material
     ${FILAMENT_PATH}/matc -a opengl -a metal -a vulkan -o materials/$material.filamat materials/$material.mat || exit 1; \
 	${FILAMENT_PATH}/resgen -c -p $material -x thermion_dart/native/include/material/ materials/$material.filamat || exit 1; \
