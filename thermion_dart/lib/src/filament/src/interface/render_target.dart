80ceb5988e0b8f8d237f391d76e29762f5268f2f
Nick Fisher <nick.fisher@avinium.com>
2025-07-01 10:04:13 +0800

    RenderTarget now implements NativeHandle

--- thermion_dart/lib/src/filament/src/interface/render_target.dart
+++ thermion_dart/lib/src/filament/src/interface/render_target.dart
@@ -1,6 +1,7 @@
+import 'package:thermion_dart/src/filament/src/interface/native_handle.dart';
 import 'package:thermion_dart/thermion_dart.dart';
 
-abstract class RenderTarget {
+abstract class RenderTarget<T> extends NativeHandle {
   Future<Texture> getColorTexture();
   Future<Texture> getDepthTexture();
   Future destroy();
