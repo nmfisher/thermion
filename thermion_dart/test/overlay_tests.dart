a2883fbee9a441c61fa2477aad51299d1c39f822
Nick Fisher <nick.fisher@avinium.com>
2025-07-01 12:17:09 +0800

    update overlay tests

--- thermion_dart/test/overlay_tests.dart
+++ thermion_dart/test/overlay_tests.dart
@@ -5,20 +5,18 @@ void main() async {
   final testHelper = TestHelper("overlay");
   await testHelper.setup();
 
-  group("overlay tests", () {
-    test('toggle grid visibility', () async {
-      await testHelper.withViewer(
-        (viewer) async {
-          
-          await viewer.setGridOverlayVisibility(true);
-          await testHelper.capture(viewer.view, "grid_visible");
-          await testHelper.capture(viewer.view, "grid_visible");
-          await viewer.setGridOverlayVisibility(false);
-          await testHelper.capture(viewer.view, "grid_hidden");          
-          await testHelper.capture(viewer.view, "grid_hidden");          
-        },
-        postProcessing: true,
-      );
-    });
+  test('toggle grid visibility', () async {
+    await testHelper.withViewer(
+      (viewer) async {
+        await viewer.setGridOverlayVisibility(true);
+        await testHelper.capture(viewer.view, "grid_visible");
+        await testHelper.capture(viewer.view, "grid_visible");
+        await viewer.setGridOverlayVisibility(false);
+        await testHelper.capture(viewer.view, "grid_hidden");          
+        await testHelper.capture(viewer.view, "grid_hidden");          
+      },
+      postProcessing: true,
+    );
   });
+
 }
