c7dfd293e220fd2c538a32bddcd5f25dea15a71d
Nick Fisher <nick.fisher@avinium.com>
2025-07-03 14:22:26 +0800

    (web) add cplusplus guards for material header

--- thermion_dart/native/include/material/grid.h
+++ thermion_dart/native/include/material/grid.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     extern const uint8_t GRID_PACKAGE[];
     extern int GRID_GRID_OFFSET;
     extern int GRID_GRID_SIZE;
+#ifdef __cplusplus
 }
+#endif
 #define GRID_GRID_DATA (GRID_PACKAGE + GRID_GRID_OFFSET)
 
 #endif
--- thermion_dart/native/include/material/outline.h
+++ thermion_dart/native/include/material/outline.h
@@ -3,11 +3,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     extern const uint8_t OUTLINE_PACKAGE[];
     extern int OUTLINE_OUTLINE_OFFSET;
     extern int OUTLINE_OUTLINE_SIZE;
+#ifdef __cplusplus
 }
+#endif
+
 #define OUTLINE_OUTLINE_DATA (OUTLINE_PACKAGE + OUTLINE_OUTLINE_OFFSET)
 
 #endif
