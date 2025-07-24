a580e85d52184fc7b100028e1473283edecd3cd2
Nick Fisher <nick.fisher@avinium.com>
2025-07-24 17:31:57 +0800

    fix: remove errant 'dart:io' import from FFIView

--- thermion_dart/lib/src/filament/src/implementation/ffi_view.dart
+++ thermion_dart/lib/src/filament/src/implementation/ffi_view.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:logging/logging.dart';
 import 'package:thermion_dart/src/filament/src/implementation/ffi_material.dart';
 import 'package:thermion_dart/src/filament/src/implementation/ffi_texture.dart';
