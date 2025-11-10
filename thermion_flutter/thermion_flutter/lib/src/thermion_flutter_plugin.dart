419214bad5068402f084df93b5049fe7b711b34b
Nick Fisher <nick.fisher@avinium.com>
2025-11-10 17:59:53 +0800

    fix: pass destroySwapChain flag through and tweak logging

--- thermion_flutter/thermion_flutter/lib/src/thermion_flutter_plugin.dart
+++ thermion_flutter/thermion_flutter/lib/src/thermion_flutter_plugin.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:thermion_dart/thermion_dart.dart';
 import 'package:thermion_flutter/src/options.dart';
 import 'platform/platform.dart';
+import 'package:logging/logging.dart';
 
 export 'platform/platform.dart' hide ThermionFlutterPluginImpl;
 
@@ -9,6 +10,7 @@ export 'platform/platform.dart' hide ThermionFlutterPluginImpl;
 /// a Flutter platform channel to create a native rendering context, resource
 /// loader and rendering surfaces.
 abstract class ThermionFlutterPlugin {
+  static late final _logger = Logger("ThermionFlutterPlugin");
   static ThermionFlutterPluginImpl? _instance;
 
   static ThermionFlutterPlugin get instance {
@@ -24,19 +26,25 @@ abstract class ThermionFlutterPlugin {
     _options = options;
   }
 
-  Future<SwapChain?> initialize();
+  Future<SwapChain?> initialize({bool destroySwapchain = true});
 
   static Future<ThermionViewer> createViewer(
       {bool destroySwapchain = true}) async {
-    final swapChain = await instance.initialize();
+    _logger.finest("Creating viewer");
+    final swapChain = await instance.initialize(destroySwapchain: destroySwapchain);
+    _logger.finest("Plugin initialized");
     final viewer = ThermionViewerFFI();
     await viewer.initialized;
+    _logger.finest("Viewer initialized");
     if (swapChain != null) {
+      _logger.finest("Registering swapchain");
       await FilamentApp.instance!.register(swapChain, viewer.view);
+      _logger.finest("Swapchain registered");
     }
 
-      await viewer.view.setRenderable(true);
-    
+    await viewer.view.setRenderable(true);
+    _logger.finest("Set view to renderable");
+
     return viewer;
   }
 }
