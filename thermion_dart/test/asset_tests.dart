ef6971974dca2ac85e7b346ec872917b2e429370
Nick Fisher <nick.fisher@avinium.com>
2025-11-19 17:34:49 +0800

    test: update asset_tests to use ViewerBuilder

--- thermion_dart/test/asset_tests.dart
+++ thermion_dart/test/asset_tests.dart
@@ -10,113 +10,129 @@ void main() async {
   await testHelper.setup();
 
   test('load/clear skybox', () async {
-    await testHelper.withViewer((viewer) async {
-      await viewer.loadSkybox(
+    await ViewerBuilder(testHelper)
+        .setRenderTargetEnabled(true)
+        .execute((result) async {
+      await result.viewer.loadSkybox(
           "file://${testHelper.testDir}/assets/default_env_skybox.ktx");
-      await testHelper.capture(viewer.view, "load_skybox");
-      await viewer.removeSkybox();
-      await testHelper.capture(viewer.view, "remove_skybox");
+      await testHelper.capture(result.viewer.view, "load_skybox");
+      await result.viewer.removeSkybox();
+      await testHelper.capture(result.viewer.view, "remove_skybox");
 
-      await viewer.setPostProcessing(true);
-      await viewer.setBloom(false, 0.01);
-      await viewer.loadSkybox(
+      await result.viewer.setPostProcessing(true);
+      await result.viewer.setBloom(false, 0.01);
+      await result.viewer.loadSkybox(
           "file://${testHelper.testDir}/assets/default_env_skybox.ktx");
-      await testHelper.capture(viewer.view, "load_skybox_with_postprocessing");
-      await viewer.removeSkybox();
+      await testHelper.capture(result.viewer.view, "load_skybox_with_postprocessing");
+      await result.viewer.removeSkybox();
       await testHelper.capture(
-          viewer.view, "remove_skybox_with_postprocessing");
-    }, createRenderTarget: true);
+          result.viewer.view, "remove_skybox_with_postprocessing");
+    });
   });
 
   test('sync load/remove gltf from uri', () async {
-    await testHelper.withViewer((viewer) async {
-      var asset = await viewer
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
+      var asset = await result.viewer
           .loadGltf("file://${testHelper.testDir}/assets/cube.gltf");
-      await viewer
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await testHelper.capture(viewer.view, "gltf_loaded");
-      await viewer.removeFromScene(asset);
-      await testHelper.capture(viewer.view, "gltf_removed");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "gltf_loaded");
+      await result.viewer.removeFromScene(asset);
+      await testHelper.capture(result.viewer.view, "gltf_removed");
+    });
   });
 
   test('async load/remove gltf from uri', () async {
-    await testHelper.withViewer((viewer) async {
-      var asset = await viewer.loadGltf(
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
+      var asset = await result.viewer.loadGltf(
           "file://${testHelper.testDir}/assets/cube.gltf",
           loadAsync: true);
-      await viewer
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await testHelper.capture(viewer.view, "gltf_async_loaded");
-      await viewer.removeFromScene(asset);
-      await testHelper.capture(viewer.view, "gltf_async_removed");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "gltf_async_loaded");
+      await result.viewer.removeFromScene(asset);
+      await testHelper.capture(result.viewer.view, "gltf_async_removed");
+    });
   });
 
   test('sync load/remove gltf from buffer', () async {
-    await testHelper.withViewer((viewer) async {
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
       var assetData =
           File("${testHelper.testDir}/assets/cube.gltf").readAsBytesSync();
-      var asset = await viewer.loadGltfFromBuffer(assetData,
+      var asset = await result.viewer.loadGltfFromBuffer(assetData,
           resourceUri: "${testHelper.testDir}/assets",
           loadResourcesAsync: false);
-      await viewer
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await testHelper.capture(viewer.view, "gltf_load_from_buffer");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "gltf_load_from_buffer");
+    });
   });
 
   test('transform gltf to unit cube', () async {
-    await testHelper.withViewer((viewer) async {
-      var asset = await viewer
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
+      var asset = await result.viewer
           .loadGltf("file://${testHelper.testDir}/assets/cube.gltf");
 
-      await viewer
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
       await asset.setTransform(Matrix4.compose(
           Vector3.zero(), Quaternion.identity(), Vector3.all(2)));
-      await testHelper.capture(viewer.view, "gltf_before_unit_cube");
+      await testHelper.capture(result.viewer.view, "gltf_before_unit_cube");
       await asset.transformToUnitCube();
-      await testHelper.capture(viewer.view, "gltf_after_unit_cube");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "gltf_after_unit_cube");
+    });
   });
 
   test('add/remove asset from scene ', () async {
-    await testHelper.withViewer((viewer) async {
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
       var asset =
-          await viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
-      await viewer
+          await result.viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await testHelper.capture(viewer.view, "asset_added");
-      await viewer.removeFromScene(asset);
-      await testHelper.capture(viewer.view, "asset_removed");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "asset_added");
+      await result.viewer.removeFromScene(asset);
+      await testHelper.capture(result.viewer.view, "asset_removed");
+    });
   });
 
   test('destroy assets', () async {
-    await testHelper.withViewer((viewer) async {
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
       var asset =
-          await viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
-      await viewer
+          await result.viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
+      await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await testHelper.capture(viewer.view, "assets_present");
-      await viewer.destroyAssets();
-      await testHelper.capture(viewer.view, "assets_destroyed");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await testHelper.capture(result.viewer.view, "assets_present");
+      await result.viewer.destroyAssets();
+      await testHelper.capture(result.viewer.view, "assets_destroyed");
+    });
   });
 
   test('add/remove bounding box', () async {
-    await testHelper.withViewer((viewer) async {
+    await ViewerBuilder(testHelper)
+        .setCameraLookAt(Vector3(0, 0, 5))
+        .execute((result) async {
       var asset =
-          await viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
-              await viewer
+          await result.viewer.loadGltf("file://${testHelper.testDir}/assets/cube.glb");
+              await result.viewer
           .loadIbl("file://${testHelper.testDir}/assets/default_env_ibl.ktx");
-      await viewer.showBoundingBox(asset);
-      await testHelper.capture(viewer.view, "show_bounding_box");
-      await viewer.hideBoundingBox(asset);
-      await testHelper.capture(viewer.view, "hide_bounding_box");
-      await viewer.hideBoundingBox(asset, destroy: true);
-      await testHelper.capture(viewer.view, "destroy_bounding_box");
-    }, cameraPosition: Vector3(0, 0, 5));
+      await result.viewer.showBoundingBox(asset);
+      await testHelper.capture(result.viewer.view, "show_bounding_box");
+      await result.viewer.hideBoundingBox(asset);
+      await testHelper.capture(result.viewer.view, "hide_bounding_box");
+      await result.viewer.hideBoundingBox(asset, destroy: true);
+      await testHelper.capture(result.viewer.view, "destroy_bounding_box");
+    });
   });
 }
