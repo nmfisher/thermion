1c910773035a06eca616bc5054b145f75740a72d
Nick Fisher <nick.fisher@avinium.com>
2026-01-02 11:25:37 +0800

    export bone visualizer

--- thermion_dart/lib/src/utils/utils.dart
+++ thermion_dart/lib/src/utils/utils.dart
@@ -3,3 +3,6 @@ library;
 export 'src/geometry.dart';
 export 'src/image.dart';
 export 'src/gizmos.dart';
+export 'src/bone_visualizer.dart';
+export 'src/bone_visualizer.dart' show BoneVisualizationController;
+export 'src/bone_data.dart';
