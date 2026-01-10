efdd2e4749f24075a545c6f5766ed785272b4eb7
Nick Fisher <nick.fisher@avinium.com>
2026-01-10 15:26:26 +0800

    fix: hide VoidCallback to stop export/import conflicts with Flutter

--- thermion_flutter/thermion_flutter/lib/thermion_flutter.dart
+++ thermion_flutter/thermion_flutter/lib/thermion_flutter.dart
@@ -3,4 +3,4 @@ library thermion_flutter;
 export 'src/thermion_flutter_plugin.dart';
 export 'src/widgets/widgets.dart';
 export 'src/options.dart';
-export 'package:thermion_dart/thermion_dart.dart';
+export 'package:thermion_dart/thermion_dart.dart' hide VoidCallback;
