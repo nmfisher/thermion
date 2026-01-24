3539fe974667e53fe3590a644b1397d94fdbd27e
Nick Fisher <nick.fisher@avinium.com>
2026-01-24 15:58:18 +0800

    fix: add #ifdef __cplusplus guards for new overlay materials

--- thermion_dart/native/include/material/silhouette.h
+++ thermion_dart/native/include/material/silhouette.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     extern const uint8_t SILHOUETTE_PACKAGE[];
     extern int SILHOUETTE_SILHOUETTE_OFFSET;
     extern int SILHOUETTE_SILHOUETTE_SIZE;
+#ifdef __cplusplus
 }
+#endif
 #define SILHOUETTE_SILHOUETTE_DATA (SILHOUETTE_PACKAGE + SILHOUETTE_SILHOUETTE_OFFSET)
 
 #endif
