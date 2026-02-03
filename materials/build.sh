7580668662736e49e900363dce449094e5bc7ec0
Nick Fisher <nick.fisher@avinium.com>
2026-02-03 12:50:38 +0800

    chore: fix material build script to add #ifdef __cplusplus guards into header files

--- materials/build.sh
+++ materials/build.sh
@@ -4,4 +4,7 @@ for material in image unlit_fixed_size grid linear_depth silhouette edge_outline
 	${FILAMENT_PATH}/resgen -c -p $material -x thermion_dart/native/include/material/ materials/$material.filamat || exit 1; \
     echo '#include "'$material'.h"' | cat - thermion_dart/native/include/material/$material.c > thermion_dart/native/include/material/$material.c.new; \
     mv thermion_dart/native/include/material/$material.c.new thermion_dart/native/include/material/$material.c; \
+    # Add #ifdef __cplusplus guards around extern "C" in the header file
+    sed -i '' 's/extern "C" {/#ifdef __cplusplus\nextern "C" {\n#endif/' thermion_dart/native/include/material/$material.h; \
+    sed -i '' 's/^}$/#ifdef __cplusplus\n}\n#endif/' thermion_dart/native/include/material/$material.h; \
 done
