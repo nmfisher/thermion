89a53f0f8ac4cf3a0705d6cea04f2100e29e5888
Nick Fisher <nick.fisher@avinium.com>
2026-02-05 16:46:08 +0800

    update materials

--- materials/build.sh
+++ materials/build.sh
@@ -8,3 +8,9 @@ for material in image unlit_fixed_size grid linear_depth silhouette edge_outline
     sed -i '' 's/extern "C" {/#ifdef __cplusplus\nextern "C" {\n#endif/' thermion_dart/native/include/material/$material.h; \
     sed -i '' 's/^}$/#ifdef __cplusplus\n}\n#endif/' thermion_dart/native/include/material/$material.h; \
 done
+
+# Compile example asset materials (no embedded resources)
+for material in customattributes solidcolor viewspace; do \
+    echo "examples/assets/$material"
+    ${FILAMENT_PATH}/matc -a opengl -a metal -a vulkan -o examples/assets/$material.filamat examples/assets/$material.mat || exit 1; \
+done
