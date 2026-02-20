15e49feccf1b4dee541db6da9e3e8a3ec1bb7bf8
Nick Fisher <nick.fisher@avinium.com>
2026-02-20 11:43:51 +0800

    test: use ViewerBuilder for image_tests.dart

--- thermion_dart/test/image_tests.dart
+++ thermion_dart/test/image_tests.dart
@@ -10,7 +10,7 @@ void main() async {
   final testHelper = TestHelper("images");
   await testHelper.setup();
   test('decode KTX', () async {
-    await testHelper.withViewer((viewer) async {
+    await ViewerBuilder(testHelper).execute((result) async {
       final ktx1Data =
           File("${testHelper.assetsDir}/default_env_skybox.ktx")
               .readAsBytesSync();
@@ -19,41 +19,45 @@ void main() async {
   });
 
   test('set background color', () async {
-    await testHelper.withViewer((viewer) async {
-      await viewer.setBackgroundColor(0, 1, 0, 1);
-      await testHelper.capture(viewer.view, "background_green");
-      await viewer.setBackgroundColor(1, 0, 0, 1);
-      await testHelper.capture(viewer.view, "background_red");
+    await ViewerBuilder(testHelper).execute((result) async {
+      await result.viewer.setBackgroundColor(0, 1, 0, 1);
+      await testHelper.capture(result.viewer.view, "background_green");
+      await result.viewer.setBackgroundColor(1, 0, 0, 1);
+      await testHelper.capture(result.viewer.view, "background_red");
     });
   });
 
   test('set background image from PNG', () async {
-    await testHelper.withViewer((viewer) async {
-      await viewer.setBackgroundImage(
+    await ViewerBuilder(testHelper).execute((result) async {
+      await result.viewer.setBackgroundImage(
           "file://${testHelper.assetsDir}/cube_texture_512x512.png");
-      await testHelper.capture(viewer.view, "background_png_image");
+      await testHelper.capture(result.viewer.view, "background_png_image");
     });
   });
 
   test('move textured quad from near plane to far plane', () async {
-    await testHelper.withViewer((viewer) async {
-      await viewer.setBackgroundImage(
+    await ViewerBuilder(testHelper).execute((result) async {
+      await result.viewer.setBackgroundImage(
           "file://${testHelper.assetsDir}/background.ktx");
-      final quad = await viewer.getBackgroundImage();
+      final quad = await result.viewer.getBackgroundImage();
       // add a cube so we can check our depth parameters
-      final asset = await viewer.createGeometry(GeometryHelper.cube());
+      final asset =
+          await result.viewer.createGeometry(GeometryHelper.cube());
       // render image at far plane
       await quad.setDepth(0.0);
-      await testHelper.capture(viewer.view, "textured_quad_far_plane");
+      await testHelper.capture(
+          result.viewer.view, "textured_quad_far_plane");
       // render the quad at the near plane
       await quad.setDepth(1.0);
-      await testHelper.capture(viewer.view, "textured_quad_near_plane");
+      await testHelper.capture(
+          result.viewer.view, "textured_quad_near_plane");
 
       // set the clear color so we can confirm it's definitely removed
       await FilamentApp.instance!.setClearOptions(0, 0, 1, 0);
-      await viewer.clearBackgroundImage(destroy: true);
+      await result.viewer.clearBackgroundImage(destroy: true);
 
-      await testHelper.capture(viewer.view, "textured_quad_removed");
+      await testHelper.capture(
+          result.viewer.view, "textured_quad_removed");
     });
   });
 }
