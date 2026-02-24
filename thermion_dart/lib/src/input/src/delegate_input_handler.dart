65b45a4018b964637405ef19f4a23093c3c3a3f4
Nick Fisher <nick.fisher@avinium.com>
2026-02-24 09:55:52 +0800

    refactor!: remove batch option from DelegateInputHandler

--- thermion_dart/lib/src/input/src/delegate_input_handler.dart
+++ thermion_dart/lib/src/input/src/delegate_input_handler.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:logging/logging.dart';
 import 'package:thermion_dart/src/input/src/implementations/fixed_orbit_camera_delegate_v2.dart';
 import 'package:thermion_dart/src/input/src/implementations/free_flight_camera_delegate_v2.dart';
 import 'package:thermion_dart/thermion_dart.dart';
@@ -16,34 +15,17 @@ abstract class InputHandlerDelegate {
 }
 
 ///
-/// An [InputHandler] that accumulates pointer/key events every frame,
-/// delegating the actual update to an [InputHandlerDelegate].
+/// An [InputHandler] that delegates input events to an [InputHandlerDelegate].
+/// Events are processed immediately without batching.
 ///
 class DelegateInputHandler implements InputHandler {
-  
   final ThermionViewer viewer;
-
-  late final _logger = Logger(this.runtimeType.toString());
-
-  Stream<List<InputEvent>> get events => _gesturesController.stream;
-
-  final _gesturesController = StreamController<List<InputEvent>>.broadcast();
-  final _events = <InputEvent>[];
   InputHandlerDelegate? delegate;
-
-  final bool batch;
-
-  bool _processing = false;
 
   DelegateInputHandler({
     required this.viewer,
     this.delegate,
-    this.batch = false,
-
-  }) {
-    FilamentApp.instance!.registerRequestFrameHook(process);
-
-  }
+  });
 
   factory DelegateInputHandler.fixedOrbit(ThermionViewer viewer,
       {double minimumDistance = 0.1,
@@ -58,8 +40,7 @@ class DelegateInputHandler implements InputHandler {
           sensitivity: sensitivity,
           minZoomDistance: minimumDistance,
           maxZoomDistance: 1000.0,
-        ),
-        batch: true);
+        ));
   }
 
   factory DelegateInputHandler.flight(
@@ -69,65 +50,19 @@ class DelegateInputHandler implements InputHandler {
     InputSensitivityOptions sensitivity = const InputSensitivityOptions(),
   }) =>
       DelegateInputHandler(
-        batch: true,
         viewer: viewer,
-        
         delegate: FreeFlightInputHandlerDelegateV2(viewer.view,
-            sensitivity: sensitivity,moveOnHover: moveOnHover),
+            sensitivity: sensitivity, moveOnHover: moveOnHover),
       );
-
-  Future<void> process() async {
-    if (delegate == null) {
-      return;
-    }
-    _processing = true;
-
-    late final Map<LogicalKey, KeyEvent> keyDown;
-    // if batch is true, we treat any tick containing keydown/keyup for the same key as a keydown
-    if (batch) {
-      late final Map<LogicalKey, KeyEvent> keyUp = {};
-      keyDown = {};
-
-      for (final event in _events) {
-        if (event is KeyEvent) {
-          switch (event.type) {
-            case KeyEventType.up:
-              keyUp[event.logicalKey] = event;
-            case KeyEventType.down:
-              keyDown[event.logicalKey] = event;
-          }
-        }
-      }
-      for (final key in keyUp.keys) {
-        _events.remove(keyDown[key]);
-        _events.remove(keyUp[key]);
-      }
-    }
-
-    await delegate!.handle(_events.sublist(0));
-    _events.clear();
-    if (batch) {
-      _events.addAll(keyDown.values);
-    }
-
-    _processing = false;
-  }
 
   @override
   Future dispose() async {
-    FilamentApp.instance!.unregisterRequestFrameHook(process);
-    delegate?.dispose();
+    await delegate?.dispose();
   }
 
   @override
   Future handle(InputEvent event) async {
-    if (_processing || delegate == null) {
-      return;
-    }
-
-    _events.add(event);
-    if (!this.batch) {
-      await process();
-    }
+    if (delegate == null) return;
+    await delegate!.handle([event]);
   }
 }
