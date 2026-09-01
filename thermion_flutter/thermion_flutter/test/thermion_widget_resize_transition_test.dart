6ea1061039edd9851e8cb70f44581469d61c09ba
Nick Fisher <nick.fisher@avinium.com>
2026-09-01 22:30:32 +0800

    fix: flip Linux OpenGL textures for Flutter coordinates

--- thermion_flutter/thermion_flutter/test/thermion_widget_resize_transition_test.dart
+++ thermion_flutter/thermion_flutter/test/thermion_widget_resize_transition_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:thermion_flutter/src/platform/src/platform_texture_descriptor.dart';
 import 'package:thermion_flutter/src/widgets/src/thermion_widget.dart';
+import 'package:thermion_flutter/src/widgets/src/thermion_widget_internal/texture_widget_builder.dart';
 
 void main() {
   testWidgets('staged replacement is mounted below the last valid frame', (
@@ -48,4 +50,45 @@ void main() {
     expect(find.byKey(currentKey), findsOneWidget);
     expect(find.byType(Stack), findsNothing);
   });
+
+  testWidgets('flagged platform texture is vertically flipped', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: buildTextureWidget(_TextureDescriptor(flipVertically: true)),
+      ),
+    );
+
+    final transform = tester.widget<Transform>(find.byType(Transform));
+    expect(transform.transform.entry(0, 0), 1);
+    expect(transform.transform.entry(1, 1), -1);
+    expect(find.byType(Texture), findsOneWidget);
+  });
+
+  testWidgets('unflagged platform texture is not transformed', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: buildTextureWidget(_TextureDescriptor(flipVertically: false)),
+      ),
+    );
+
+    expect(find.byType(Transform), findsNothing);
+    expect(find.byType(Texture), findsOneWidget);
+  });
+}
+
+class _TextureDescriptor extends PlatformTextureDescriptor {
+  _TextureDescriptor({required bool flipVertically})
+    : super(
+        flutterTextureId: 1,
+        hardwareId: 2,
+        width: 3,
+        height: 4,
+        flipVertically: flipVertically,
+      );
+
+  @override
+  Future<void> destroy() async {}
+
+  @override
+  Future<void> markTextureFrameAvailable() async {}
 }
